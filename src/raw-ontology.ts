7dafe74a0e81178ac4ab17f7712ffe43db2d5894
avi tshuva <dharmax@gmail.com>
2021-07-02 19:34:59 +0300

    added: concatenation of raw ontologies changed: node version compatibility

--- src/raw-ontology.ts
+++ src/raw-ontology.ts
@@ -3,4 +3,12 @@ import {EntityDcr, PredicateDcr} from "./descriptors";
 export class IRawOntology {
     entityDcrs: EntityDcr[]
     predicateDcrs: PredicateDcr[]
+
+    concat(...ont:IRawOntology[]):IRawOntology {
+        for ( let o of ont) {
+            this.entityDcrs.push( ...o.entityDcrs)
+            this.predicateDcrs.push( ...o.predicateDcrs)
+        }
+        return this
+    }
 }
